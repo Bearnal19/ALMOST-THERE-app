303eccd2a30848aab0668da8f977de2f3537b8ea
Bearnal19 <cesar_enigma19@hotmail.com>
2019-05-26 18:03:37 -0500

    Machote de vista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },  { path: 'add-alarm', loadChildren: './add-alarm/add-alarm.module#AddAlarmPageModule' }
+
 ];
 
 @NgModule({
