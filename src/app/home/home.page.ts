e24469dcb5da5c68d03327fe0e1e836414b73a1f
MAngelJN <mangeljn@gmail.com>
2019-06-02 17:38:06 -0500

    Componente alarms y navegacion entre paginas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(private navCtrl: NavController){
+
+  }
+  addItemAlarm(){
+    this.navCtrl.navigateForward("list");
+  }
 }
